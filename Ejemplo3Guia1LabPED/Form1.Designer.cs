081c227917d1fd41f5b51f010c6357e1527578f2
vicastillo <vicente468@outlook.com>
2022-01-31 22:20:39 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo3Guia1LabPED/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Ejemplo3Guia1LabPED
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(401, 402);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "Circulo",
+            "Rectangulo"});
+            this.listBox1.Location = new System.Drawing.Point(436, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(221, 79);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 442);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Ejemplo3Guia1LabPED/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo3Guia1LabPED
+{
+    public partial class Form1 : Form
+    {
+
+        int x, y; //variables globales que permiten determinar ubicacion del click
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point punto = new Point(e.X, e.Y);
+            x = punto.X;
+            y = punto.Y;
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics(); //establece zona para dibujo
+            Pen lapiz = new Pen(Color.Black); //declaro color de pen a utilizar
+
+            if(listBox1.SelectedIndex==0) //si selecciona circulo
+            {
+            SolidBrush sb = new SolidBrush(Color.Red); //brush en rojo
+            g.DrawEllipse(lapiz, x - 50, y - 50, 100, 100); /*dibujara elipse utilizando ese pen,
+            con la posiicion en X y Y menos 50 y dimensiones 100 */
+                g.FillEllipse(sb, x - 50, y - 50, 100, 100); //rellena de color la elipse dada
+            }
+
+            if (listBox1.SelectedIndex == 1) //si selecciona rectangulo
+            {
+            SolidBrush sb = new SolidBrush(Color.Green); //brush en verde
+            g.DrawRectangle(lapiz, x - 50, y - 50, 100, 100); /*dibujara rectangulo utilizando ese pen,
+            con la posicion en X y Y menos 50 y dimensiones 100 */
+            g.FillRectangle(sb, x - 50, y - 50, 100, 100); //rellena de color la elipse dada
+            }
+        }
+    }
+}
